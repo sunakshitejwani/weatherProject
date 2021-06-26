19f3c32c431f3b7e8a52c7c685e222c105311493
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-26 21:21:37 +0530

    use body parser to post requests to the server

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  console.log();
+  const city = req.body.cityName;
+  const apiKey = "d6fc3936c2f90af6cbf38861a63e5d05";
+  const unit = "metric";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=jaipur&appid=d6fc3936c2f90af6cbf38861a63e5d05&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
   https.get(url, function(response) {
     console.log(response.statusCode);
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
-      console.log(weatherData);
       const temp = weatherData.main.temp;
-      console.log(temp);
       const icon = weatherData.weather[0].icon;
-      console.log(icon);
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       const weatherDescription = weatherData.weather[0].description;
       res.write(
@@ -23,7 +36,7 @@ app.get("/", function(req, res) {
       res.write(
         "<h1>The temperature in Jaipur is " + temp + " degrees Celcius.</h1>"
       );
-      res.write("<img src=" + imageURL +">");
+      res.write("<img src=" + imageURL + ">");
       res.send();
     });
   });
